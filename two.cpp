d1b508964e0188c51dde08326e49fb05b33df30a
nikita-mirchandani <82458994+nikita-mirchandani@users.noreply.github.com>
2022-03-25 16:57:29 +0530

    two.cpp

---
+++ two.cpp
@@ -0,0 +1,26 @@
+//2. Find the minimum and maximum element in an array
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,max=INT_MIN,min=INT_MAX;
+    cin>>n;
+    int array[n];
+    // int n = sizeof(array)/sizeof(int);
+    for(int i=1;i<=n;i++){
+        cin>>array[i];
+    }
+     for(int i=1;i<=n;i++){ //max = -1
+       if(array[i]>=max){
+           max = array[i];
+       }
+       if(array[i]<=min){
+           min = array[i];
+       }
+    }
+    // for(int i=1;i<=n;i++){
+    //     cout<<array[i]<<" ";
+    // }
+    cout<<"Maximum element in array is "<<max<<endl;
+    cout<<"Minimum element in array is "<<min<<endl;
+
+}
